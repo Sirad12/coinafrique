2c408253c5d3593bde7d4451a52f364657a6a498
Sirad12 <siradia167@gmail.com>
2026-01-10 19:55:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -381,9 +381,10 @@ elif menu == "Évaluation":
         st.markdown(""" 
         <div style="border:1px solid #ccc; padding:15px; border-radius:10px; background-color:#F1F5F9; text-align:center"> 
         <h4>📋 Formulaire Google </h4> 
-        <a href="https://forms.gle/SE3yPxVg8Zu8FwHp9" target="_blank" style="font-size:16px; font-weight:bold; color:#1E3A8A"> 
+        <a href="https://forms.gle/SE3yPxVg8Zu8FwHp9" target="_blank" style="font-size:16px; font-weight:bold; color: #E0F2FE"> 
           Accéder au formulaire google </a> 
         </div> """, unsafe_allow_html=True)
+
 
 
 
