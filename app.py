a875291525d3fab91da46aebb4cb5b4b05c9cb54
Sirad12 <siradia167@gmail.com>
2026-01-10 17:49:29 +0000

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ elif menu == "Évaluation":
         st.markdown(""" 
         <div style="border:1px solid #ccc; padding:15px; border-radius:10px; background-color:#f9f9f9"> 
         <h4>📋 Formulaire KoboToolbox</h4> 
-        <a href="https://ee.kobotoolbox.org/x/jfxd3Sgy" target="_blank" style="font-size:16px; font-weight:bold; color:#F1F5F9"> 
+        <a href="https://ee.kobotoolbox.org/x/jfxd3Sgy" target="_blank" style="font-size:16px; font-weight:bold; color:#1E3A8A"> 
           Accéder au formulaire KoboToolbox </a> 
         </div> """, unsafe_allow_html=True)
 
