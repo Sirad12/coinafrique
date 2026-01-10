b645436395b319f951513ae76a130e028dad4f5e
Sirad12 <siradia167@gmail.com>
2026-01-10 17:57:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.set_page_config(
     layout="wide"
 )
 
-COLOR_PRIX = "#F4A7B9"     #rose
-COLOR_VILLES =  "#A7C7E7"   # bleu ciel
+COLOR_PRIX = "#F4A7B9"     # rose
+COLOR_VILLES = #000080     # bleu marine
 COLOR_MOYEN = "#A8D5BA"     # vert menthe
 
 
@@ -244,6 +244,7 @@ elif menu == "Évaluation":
         <a href="https://ee.kobotoolbox.org/x/jfxd3Sgy" target="_blank" style="font-size:16px; font-weight:bold; color:#1E3A8A"> 
           Accéder au formulaire KoboToolbox </a> 
         </div> """, unsafe_allow_html=True)
+
 
 
 
